d73af78480f78e1a0755d41c5b128b5a98eef344
next2014 <83329265@qq.com>
2020-10-04 10:09:45 +0800

    first commit

---
+++ src/api/edu/video.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+
+export default {
+
+    //添加章节
+    addVideo(video) {
+        return request({
+            url: '/eduservice/video/addVideo',
+            method: 'post',
+            data: video
+        })
+    },
+    
+    //删除小节
+    dalateVideo(id) {
+        return request({
+            url: '/eduservice/video/'+id,
+            method: 'delete'
+        })
+    },
+
+    //根据章节id查询
+   getVideo(id) {
+        return request({
+        url: '/eduservice/video/getVideo/'+id,
+        method: 'get'
+        })
+    },
+    //修改章节
+    updateVideo(video) {
+        return request({
+            url: '/eduservice/video/updateVideo',
+            method: 'post',
+            data: video
+        })
+    },
+
+    
+}
