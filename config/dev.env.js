3aa5e51aff8822f9359b98eade1069343fcdff19
next2014 <83329265@qq.com>
2020-10-04 10:25:05 +0800

    1004

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,4 +6,20 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
  BASE_API: '"http://localhost:9004"', //nginx 设置接口跳转地址
+
+/////////// nginx 配置 //////////
+
+//  server {
+//   listen       9004;
+//   server_name  localhost;
+
+//   location ~ /eduservice/ {
+//     proxy_pass http://localhost:8001;
+//   }
+
+//   location ~ /eduoss/ {
+//     proxy_pass http://localhost:8002;
+//   }
+// }
+
 })
